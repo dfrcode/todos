7fa96924420d80e03d033bbf2e2c1990f8578854
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-13 13:12:33 +0300

    fix styles AppTodoList, add buttons trash, important

--- src/components/AppTodoList/AppTodoList.js
+++ src/components/AppTodoList/AppTodoList.js
@@ -2,15 +2,67 @@ import React from "react";
 
 import "./AppTodoList.css";
 
+import { FaTrashAlt, FaExclamation } from "react-icons/fa";
+
 const AppTodoList = () => {
   return (
     <div className="todo-list">
       <ul className="list">
-        <li className="list-item">Learn HTML5</li>
-        <li className="list-item">Learn CSS3</li>
-        <li className="list-item">Learn JavaScript</li>
-        <li className="list-item">Learn React</li>
-        <li className="list-item">Learn Redux</li>
+        <li className="list-item">
+          <span className="item-label">Learn HTML5</span>
+          <ul className="list-fa">
+            <li className="list-fa-item trash">
+              <FaTrashAlt />
+            </li>
+            <li className="list-fa-item important">
+              <FaExclamation />
+            </li>
+          </ul>
+        </li>
+        <li className="list-item">
+        <span className="item-label">Learn CSS3</span>
+          <ul className="list-fa">
+            <li className="list-fa-item trash">
+              <FaTrashAlt />
+            </li>
+            <li className="list-fa-item important">
+              <FaExclamation />
+            </li>
+          </ul>
+        </li>
+        <li className="list-item">
+        <span className="item-label">Learn JavaScript</span>
+          <ul className="list-fa">
+            <li className="list-fa-item trash">
+              <FaTrashAlt />
+            </li>
+            <li className="list-fa-item important">
+              <FaExclamation />
+            </li>
+          </ul>
+        </li>
+        <li className="list-item">
+        <span className="item-label">Learn React</span>
+          <ul className="list-fa">
+            <li className="list-fa-item trash">
+              <FaTrashAlt />
+            </li>
+            <li className="list-fa-item important">
+              <FaExclamation />
+            </li>
+          </ul>
+        </li>
+        <li className="list-item">
+        <span className="item-label">Learn Redux</span>
+          <ul className="list-fa">
+            <li className="list-fa-item trash">
+              <FaTrashAlt />
+            </li>
+            <li className="list-fa-item important">
+              <FaExclamation />
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
