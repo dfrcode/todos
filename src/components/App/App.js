99bb63f7a31f1df2bc9e0fa0c708e0cc110749e0
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-13 14:53:23 +0300

    add items in state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,18 +8,53 @@ import AppTodoList from "../AppTodoList";
 import AppAddItemPanel from "../AppAddItemPanel";
 
 class App extends Component {
+
   state = {
     title: "Todo List",
     searchValue: "",
     addItemValue: "",
+    todoData: [
+      {
+        id: 1,
+        label: "Learn HTML5",
+        done: false,
+        important: false,
+      },
+      {
+        id: 2,
+        label: "Learn CSS3",
+        done: false,
+        important: false,
+      },
+      {
+        id: 3,
+        label: "Learn JavaScript",
+        done: false,
+        important: false,
+      },
+      {
+        id: 4,
+        label: "Learn React",
+        done: false,
+        important: false,
+      },
+      {
+        id: 5,
+        label: "Learn Redux",
+        done: false,
+        important: false,
+      },
+    ],
   };
 
+  // text in search panel
   enterLabelSearch = (e) => {
     this.setState({
       searchValue: e.target.value,
     });
   };
 
+  // text in add item panel
   addLabelItem = (e) => {
     this.setState({
       addItemValue: e.target.value,
@@ -27,7 +62,7 @@ class App extends Component {
   };
 
   render() {
-    const { title, searchValue, addItemValue } = this.state;
+    const { title, searchValue, addItemValue, todoData } = this.state;
     const { enterLabelSearch, addLabelItem } = this;
     return (
       <div className="container">
@@ -36,8 +71,8 @@ class App extends Component {
           value={searchValue}
           enterLabelSearch={enterLabelSearch}
         />
-        <AppTodoList />
-        <AppAddItemPanel value={addItemValue} addLabelItem={addLabelItem}/>
+        <AppTodoList todoData={todoData}/>
+        <AppAddItemPanel value={addItemValue} addLabelItem={addLabelItem} />
       </div>
     );
   }
--- src/components/AppTodoList/AppTodoList.js
+++ src/components/AppTodoList/AppTodoList.js
@@ -4,66 +4,25 @@ import "./AppTodoList.css";
 
 import { FaTrashAlt, FaExclamation } from "react-icons/fa";
 
-const AppTodoList = () => {
-  return (
-    <div className="todo-list">
-      <ul className="list">
-        <li className="list-item done">
-          <span className="item-label">Learn HTML5</span>
-          <ul className="list-fa">
-            <li className="list-fa-item trash">
-              <FaTrashAlt />
-            </li>
-            <li className="list-fa-item important">
-              <FaExclamation />
-            </li>
-          </ul>
+const AppTodoList = (props) => {
+  const { todoData } = props;
+  const elements = todoData.map((todo) => (
+    <li key={todo.id} className="list-item">
+      <span className="item-label">{todo.label}</span>
+      <ul className="list-fa">
+        <li className="list-fa-item trash">
+          <FaTrashAlt />
         </li>
-        <li className="list-item important">
-        <span className="item-label">Learn CSS3</span>
-          <ul className="list-fa">
-            <li className="list-fa-item trash">
-              <FaTrashAlt />
-            </li>
-            <li className="list-fa-item important">
-              <FaExclamation />
-            </li>
-          </ul>
-        </li>
-        <li className="list-item">
-        <span className="item-label">Learn JavaScript</span>
-          <ul className="list-fa">
-            <li className="list-fa-item trash">
-              <FaTrashAlt />
-            </li>
-            <li className="list-fa-item important">
-              <FaExclamation />
-            </li>
-          </ul>
-        </li>
-        <li className="list-item">
-        <span className="item-label">Learn React</span>
-          <ul className="list-fa">
-            <li className="list-fa-item trash">
-              <FaTrashAlt />
-            </li>
-            <li className="list-fa-item important">
-              <FaExclamation />
-            </li>
-          </ul>
-        </li>
-        <li className="list-item">
-        <span className="item-label">Learn Redux</span>
-          <ul className="list-fa">
-            <li className="list-fa-item trash">
-              <FaTrashAlt />
-            </li>
-            <li className="list-fa-item important">
-              <FaExclamation />
-            </li>
-          </ul>
+        <li className="list-fa-item important">
+          <FaExclamation />
         </li>
       </ul>
+    </li>
+  ));
+
+  return (
+    <div className="todo-list">
+      <ul className="list">{elements}</ul>
     </div>
   );
 };
