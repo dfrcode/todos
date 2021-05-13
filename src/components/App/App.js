28af2094c7257c6ae3915e430328b2f1d9e2f1b4
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-13 10:06:13 +0300

    add folder components and first component App

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>New application...</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return(
-    <div className="container">
-      <h1>New application...</h1>
-    </div>
-  )
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById("root"));
